1347a982e2075df149ff755b687338a82d7f7e53
Karunasindhu Adak <karunasindhuadak7@gmail.com>
2025-10-17 02:51:12 +0530

    new change

--- 05passwordGenerator/vite.config.js
+++ 05passwordGenerator/vite.config.js
@@ -4,8 +4,10 @@ import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: '/my-first-react-app/05passwordGenerator/',  // add this line
   plugins: [
     tailwindcss(),
     react()
   ],
 })
+
